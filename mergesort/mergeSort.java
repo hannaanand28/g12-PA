7a74fedc8a2b0f3b6f89583cc311559f1e252c40
hannaanand28 <hanna.anand06@gmail.com>
2026-08-03 11:47:05 +0000

    add

---
+++ mergesort/mergeSort.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Main {
+    public static void mergeSort(int[] arr,int low,int high){
+        if(low>=high){
+            return;
+        }
+        int mid = low+(high-low)/2;
+        // why this is right way not low+high/2 :-> this method is not valid because when we take very large values because vo intger ki value se bhi upar ja sakta h isliye 
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+    
+    static void merge(int[] arr,int low,int mid,int high){
+        int[] temp=new int[high-low+1];
+        int left=low;
+        int right=mid+1;
+        int k=0;
+        while(left<=mid && right<=high){
+            if(arr[left] <= arr[right]){
+                temp[k++]=arr[left++];
+            }else{
+                temp[k++]=arr[right++];
+            }
+            
+        }
+        while(left<=mid){
+            temp[k++]=arr[left++];
+        }
+        while(right<=high){
+            temp[k++]=arr[right++];
+        }
+        for(int i=low;i<=high;i++){
+            arr[i]=temp[i-low];
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr= {10,4,6,1,11,3,2,8};
+        mergeSort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ quicksort/quicksort.java
@@ -0,0 +1,37 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void quickSort(int[] arr,int low,int high){
+        if(low<high){
+            int pivotIndex = parition(arr,low,high);
+            quickSort(arr,low,pivotIndex-1);
+            quickSort(arr,pivotIndex+1,high);
+        }
+    }
+    static int parition(int arr[],int low,int high){
+        int pivot = arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1]=arr[high];
+        arr[high]=temp;
+        return i+1;
+    }
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        int[] arr = {7,2,9,1,5,8,3};
+        quickSort(arr,0,arr.length-1);
+        for (int x : arr) { 
+            System.out.print(x + " ");
+        }
+    }
+}
+
